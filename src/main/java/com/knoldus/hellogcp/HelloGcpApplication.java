e368ad82e229cdba5ce1956d0a13c382c754758b
knoldus <riya.sharma@knoldus.com>
2023-07-06 16:35:10 +0530

    "Initial commit"

---
+++ src/main/java/com/knoldus/hellogcp/HelloGcpApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.hellogcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGcpApplication.class, args);
+	}
+
+}
